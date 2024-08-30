9432d0c59c89cb85c193f2214421e2566886d660
Nikita Suslov <nikitasuslov666@gmail.com>
2024-08-30 04:07:46 +0300

    Add Decimal to Binary function

---
+++ bit.js
@@ -0,0 +1,9 @@
+function convert(decimal_number) {
+  let binary_number = "";
+  while (Math.floor(decimal_number) > 1) {
+    binary_number += decimal_number % 2;
+    decimal_number = Math.floor(decimal_number / 2);
+  }
+  binary_number += decimal_number;
+  return binary_number.split("").reverse().join("");
+}
