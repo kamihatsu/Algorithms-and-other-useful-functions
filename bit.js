403b94b733de878aaa956fa2c2459a352b2e78ea
Nikita Suslov <70162206+kamihatsu@users.noreply.github.com>
2024-08-30 12:22:07 +0300

    Update bit.js

--- bit.js
+++ bit.js
@@ -1,9 +1,6 @@
-function convert(decimal_number) {
+unction convert_decimal_to_binary(decimal_number) {
   let binary_number = "";
   while (Math.floor(decimal_number) > 1) {
     binary_number += decimal_number % 2;
-    decimal_number = Math.floor(decimal_number / 2);
-  }
-  binary_number += decimal_number;
-  return binary_number.split("").reverse().join("");
-}
+    decimal_number = Math.floor(decimal_number / 2);                                                  }
+  binary_number += decimal_number;                  return binary_number.split("").reverse().join("");                                                }                                                                                                                                                                
